f3bf67304324816abb84d5d28e4d0833010ab5f9
Ahmed Usama <ausama@innotech.com.eg>
2024-10-29 17:34:26 +0300

    Implementing searching functionality

--- XUnitWithCRUD/Controllers/PersonsController.cs
+++ XUnitWithCRUD/Controllers/PersonsController.cs
@@ -14,8 +14,21 @@ public class PersonsController : Controller
     }
     [Route("persons/index")]
     [Route("/")]
-    public IActionResult Index()
-    {   List<PersonResponse> persons = _personService.GetAllPersons();
+    public IActionResult Index(string searchBy , string? searchString)
+    {
+        ViewBag.SearchFields = new Dictionary<string, string>()
+         {
+            { nameof(PersonResponse.PersonName), "Person Name" },
+            { nameof(PersonResponse.Email), "Email" },
+            { nameof(PersonResponse.DateOfBirth), "Date of Birth" },
+            { nameof(PersonResponse.Gender), "Gender" },
+            { nameof(PersonResponse.CountryId), "Country" },
+            { nameof(PersonResponse.Address), "Address" }
+         };
+
+        List<PersonResponse> persons = _personService.GetFilteredPersons(searchBy , searchString);
+        ViewBag.CurrentSearchBy = searchBy;
+        ViewBag.CurrentSearchString = searchString;
         return View(persons);
     }
 }
