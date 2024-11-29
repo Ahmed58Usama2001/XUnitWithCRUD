bbe1f1511f1d13d12af0e1dd6b1723d5ffd17b02
Ahmed Usama <ausama@innotech.com.eg>
2024-11-29 14:42:55 +0200

    Seed data

--- Entities/PersonsDbContext.cs
+++ Entities/PersonsDbContext.cs
@@ -13,5 +13,19 @@ public class PersonsDbContext:DbContext
 
         modelBuilder.Entity<Country>().ToTable("Countries");
         modelBuilder.Entity<Person>().ToTable("Persons");
+
+        string CountriesJson = System.IO.File.ReadAllText("countries.json");
+        List<Country> Countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(CountriesJson)??new();
+        foreach (var country in Countries)
+            modelBuilder.Entity<Country>().HasData(country);
+
+        string PersonsJson = System.IO.File.ReadAllText("persons.json");
+        List<Person> Persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(PersonsJson) ?? new();
+        foreach (var person in Persons)
+            modelBuilder.Entity<Person>().HasData(person);
+
+
+
+
     }
 }
