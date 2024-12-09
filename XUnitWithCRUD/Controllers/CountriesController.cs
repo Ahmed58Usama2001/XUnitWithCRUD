5b1a105dd2cedb1626533ca48a6bb4346d564d5e
Ahmed Usama <ausama@innotech.com.eg>
2024-12-09 11:48:02 +0200

    Excel to database view

---
+++ XUnitWithCRUD/Controllers/CountriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XUnitWithCRUD.Controllers;
+
+[Route("[controller]")]
+public class CountriesController : Controller
+{
+    [Route("UploadFromExcel")]
+    public IActionResult UploadFromExcel()
+    {
+        return View();
+    }
+}
